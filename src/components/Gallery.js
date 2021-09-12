42aed98473b84bd9b2ad5b4240b361d7d4ecde5a
leenahmad <leenkaraja24@gmail.com>
2021-09-12 20:09:07 +0300

    lab1 done

---
+++ src/components/Gallery.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Main from "./Main";
+
+
+class Gallery extends React.Component{
+    render(){
+        return(
+          <Main Beasts = {''}/>
+        );
+    }
+}
+
+
+export default Gallery;
