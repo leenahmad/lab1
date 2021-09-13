c5994907189ae7aacff305718ad2a463cae75574
leenahmad <leenkaraja24@gmail.com>
2021-09-13 20:26:45 +0300

    class 2 done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,15 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class Footer extends React.Component {
   render() {
-    return <h3> Author : Leen Ahmad</h3>;
+    return (
+      <div class="text-center">
+        <div class="p-3 mb-2 bg-dark text-white">
+          <h3> Author : Leen Ahmad</h3>
+        </div>
+      </div>
+    );
   }
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,15 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class Header extends React.Component {
   render() {
-    return <h1> Gallery Of Horns ! hi</h1>;
+    return (
+      <div class="text-center">
+        <div class="p-3 mb-2 bg-dark text-white">
+          <h1>Gallery Of Horns ! hi</h1>
+        </div>
+      </div>
+    );
   }
 }
 
