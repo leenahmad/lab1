5832cd28756d8e518506b43f4a734b291c05c2ad
leenahmad <leenkaraja24@gmail.com>
2021-09-13 15:46:35 +0300

    edit json

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Header from "./components/Header";
-// import Main from "./components/Main";
-import Gallery from "./components/Gallery";
+import Main from "./components/Main";
 import Footer from "./components/Footer";
+
 
 class App extends React.Component {
   render() {
@@ -10,7 +10,7 @@ class App extends React.Component {
       <>
         <Header />
 
-        <Gallery />
+        < Main/>
 
         <Footer />
       </>
